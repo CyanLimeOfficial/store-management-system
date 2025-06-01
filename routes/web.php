c4950e55435dcdae5ab72a4b1792ad22b1e1163f
CyanLimePercival <jamesataturuk@hmail.com>
2025-06-02 00:07:17 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/home', function () {
+    return view('dashboard.index');
+})->middleware('auth');
